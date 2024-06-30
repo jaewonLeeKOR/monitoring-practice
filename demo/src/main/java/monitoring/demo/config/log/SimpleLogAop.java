3cef14fd5e3940acd84f0ef78a144304a96c1661
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2024-06-30 20:41:23 +0900

    add sleuth dependency

--- demo/src/main/java/monitoring/demo/config/log/SimpleLogAop.java
+++ demo/src/main/java/monitoring/demo/config/log/SimpleLogAop.java
@@ -3,7 +3,9 @@ package monitoring.demo.config.log;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
@@ -14,6 +16,7 @@ import java.lang.reflect.Method;
 @Aspect
 @Component
 public class SimpleLogAop {
+    private StopWatch stopWatch;
     @AfterThrowing(pointcut = "execution(* monitoring.demo..*(..))", throwing = "exception")
     public void afterThrowingLog(JoinPoint joinPoint, Exception exception) {
         Method method = getMethod(joinPoint);
@@ -24,16 +27,17 @@ public class SimpleLogAop {
     public Object controllerLog(ProceedingJoinPoint joinPoint) throws Throwable {
         Method method = getMethod(joinPoint);
         StringBuilder parameters = new StringBuilder();
-        parameters.append(String.format("BEFORE[%d] %s.%s => ", Thread.currentThread().getId(), joinPoint.getTarget().getClass().getSimpleName(), method.getName()));
-        StopWatch stopWatch = new StopWatch();
+        stopWatch = new StopWatch();
+        parameters.append(String.format("BEFORE[%d](%f) %s.%s => ", Thread.currentThread().getId(), stopWatch.getTotalTimeSeconds(), joinPoint.getTarget().getClass().getSimpleName(), method.getName()));
         stopWatch.start();
         for (Object arg : joinPoint.getArgs()) {
             parameters.append(arg).append(", ");
         }
-        stopWatch.stop();
         log.trace(parameters.toString());
         Object returnObj = joinPoint.proceed();
+        stopWatch.stop();
         log.trace(String.format("AFTER[%d](%f) %s.%s => %s", Thread.currentThread().getId(), stopWatch.getTotalTimeSeconds(), joinPoint.getTarget().getClass().getSimpleName(), method.getName(), returnObj));
+        stopWatch = null;
         return returnObj;
     }
 
@@ -41,13 +45,10 @@ public class SimpleLogAop {
     public Object serviceLog(ProceedingJoinPoint joinPoint) throws Throwable {
         Method method = getMethod(joinPoint);
         StringBuilder parameters = new StringBuilder();
-        parameters.append(String.format("BEFORE[%d] %s.%s => ", Thread.currentThread().getId(), joinPoint.getTarget().getClass().getSimpleName(), method.getName()));
-        StopWatch stopWatch = new StopWatch();
-        stopWatch.start();
+        parameters.append(String.format("BEFORE[%d](%f) %s.%s => ", Thread.currentThread().getId(), stopWatch.getTotalTimeSeconds(), joinPoint.getTarget().getClass().getSimpleName(), method.getName()));
         for (Object arg : joinPoint.getArgs()) {
             parameters.append(arg).append(", ");
         }
-        stopWatch.stop();
         log.trace(parameters.toString());
         Object returnObj = joinPoint.proceed();
         log.trace(String.format("AFTER[%d](%f) %s.%s => %s", Thread.currentThread().getId(), stopWatch.getTotalTimeSeconds(), joinPoint.getTarget().getClass().getSimpleName(), method.getName(), returnObj));
