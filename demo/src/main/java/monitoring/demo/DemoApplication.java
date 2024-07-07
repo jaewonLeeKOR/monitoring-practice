eb6d0841e386b627571ccca64acecb0a125185be
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2024-07-08 01:28:36 +0900

    서버 시간대 변경

--- demo/src/main/java/monitoring/demo/DemoApplication.java
+++ demo/src/main/java/monitoring/demo/DemoApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+import java.time.ZoneId;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableAspectJAutoProxy
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of("Asia/Seoul")));
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
